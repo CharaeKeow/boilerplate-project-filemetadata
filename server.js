b0af5f85a4719fc12e8fd55ed856da6e5d43e6a1
CharaeKeow <charaekeow11@gmail.com>
2021-09-09 23:46:47 +0800

    add upload file using multer function

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-var app = express();
+var app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
-
-
+//POST /api/fileanalyse to upload the file and return
+//name, type, and size
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let file = req.file //var to hold the uploaded file data
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
